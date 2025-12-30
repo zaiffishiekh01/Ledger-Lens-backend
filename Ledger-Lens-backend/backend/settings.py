fc82a3d455fd0574ba26da1053d7199c35285d90
huzaifa <huzaifasaqib420@gmail.com>
2025-12-30 15:54:20 +0500

    Add .env.example file

--- Ledger-Lens-backend/backend/settings.py
+++ Ledger-Lens-backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-s3kmjjo2p0jn5f+j2gxfz52910fg&p6%umzpd8exx3)xc8#u)n')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
